859d183b312d6eee22d0e749cb95a27d24061fb2
Adam Skoczeń <adamsk91@wp.pl>
2023-02-01 19:53:02 +0100

    css complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
-import './css/common.css';
+import '../css/formGallery.css';
 
-console.log('cos')
+console.log('start');
+
+const inputName = document.querySelector('[name="searchQuery"]');
+const searchButton = document.querySelector(".searchButton");
+const galleryContainer = document.querySelector(".gallery");
+
+searchButton.addEventListener("submit", () => {
+    fetch(`https://pixabay.com/api/?key33296379-dab9a6cf136214dcc7548d115&q=${inputName.value}&image_type=photo`);
+});
