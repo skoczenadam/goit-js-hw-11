b4ba755ea2e4834c2e10b1adf4f5b26faae25054
skoczenadam <109891766+skoczenadam@users.noreply.github.com>
2023-02-01 18:53:22 +0000

    Deploying to gh-pages from @ skoczenadam/goit-js-hw-11@859d183b312d6eee22d0e749cb95a27d24061fb2 🚀

---
+++ index.176f54b8.js
@@ -0,0 +1,2 @@
+!function(){console.log("start");var e=document.querySelector('[name="searchQuery"]'),t=document.querySelector(".searchButton");document.querySelector(".gallery");t.addEventListener("submit",(function(){fetch("https://pixabay.com/api/?key33296379-dab9a6cf136214dcc7548d115&q=".concat(e.value,"&image_type=photo"))}))}();
+//# sourceMappingURL=index.176f54b8.js.map
---
+++ index.71504e29.js
@@ -0,0 +1,2 @@
+console.log("start");const e=document.querySelector('[name="searchQuery"]'),t=document.querySelector(".searchButton");document.querySelector(".gallery");t.addEventListener("submit",(()=>{fetch(`https://pixabay.com/api/?key33296379-dab9a6cf136214dcc7548d115&q=${e.value}&image_type=photo`)}));
+//# sourceMappingURL=index.71504e29.js.map
