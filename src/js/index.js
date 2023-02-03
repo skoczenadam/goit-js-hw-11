30ea44d52c1742114beb9f70d5063f1eee69d3b6
Adam Skoczeń <adamsk91@wp.pl>
2023-02-03 21:56:32 +0100

    almost finished

--- src/js/index.js
+++ src/js/index.js
@@ -7,29 +7,21 @@ const inputName = document.querySelector('[name="searchQuery"]');
 const searchButton = document.querySelector(".searchButton");
 const galleryContainer = document.querySelector(".gallery");
 
-fetch(`https://pixabay.com/api/?key=33296379-dab9a6cf136214dcc7548d115&q=yellow+flowers&image_type=photo`, {
-    method: 'GET',
-    
-})
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(error => console.log("Error: ", error));
-
 searchButton.addEventListener("click", e => {
     e.preventDefault();
   let itemName = inputName.value.trim();
   if (itemName === "") return galleryContainer.innerHTML = "";
   
   fetchItems(itemName)
-    .then(items => renderItems(items))
+      .then(items => renderItems(items))
     .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no item with that name');
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        console.log(error);
     });
 });
 
 function fetchItems(itemName) {
-    // return fetch(`https://pixabay.com/api/?key33296379-dab9a6cf136214dcc7548d115&q=${itemtName.value}&image_type=photo`)
-    return fetch(`https://pixabay.com/api/?key=33296379-dab9a6cf136214dcc7548d115&q=yellow+flowers&image_type=photo`)
+    return fetch(`https://pixabay.com/api/?key=33296379-dab9a6cf136214dcc7548d115&q=${itemName}&image_type=photo`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
@@ -39,26 +31,21 @@ function fetchItems(itemName) {
 };
 
 function renderItems(items) {
-    const markup = items
-        .map((item) => {
-          return `<div class="photo-card">
-                <img src="${item.webformatURL}" alt="" loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                    <b>Likes</b>
-                    </p>
-                    <p class="info-item">
-                    <b>Views</b>
-                    </p>
-                    <p class="info-item">
-                    <b>Comments</b>
-                    </p>
-                    <p class="info-item">
-                    <b>Downloads</b>
-                    </p>
-                </div>
-            </div>`;
+    console.log(items);
+    const markup = items.hits
+        .map(({webformatURL, likes, views, comments, downloads}) => {
+            return `<div class="photo-card"> 
+          <img src="${webformatURL}" alt="" loading="lazy" /> 
+          <div class="info">
+          <p class="info-item"> <b>Likes</b> ${likes} </p> 
+          <p class="info-item"> <b>Views</b> ${views} </p> 
+          <p class="info-item"> <b>Comments</b> ${comments} </p> 
+          <p class="info-item"> <b>Downloads</b> ${downloads} </p> 
+          </div> 
+          </div>`;
         }).join("");
   
     galleryContainer.innerHTML = markup;
+    console.log("działa")
 };
+
