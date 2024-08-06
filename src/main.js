7d0604d7bfa1d2227d56e75032e4fe8cc702dd92
Adam Skoczeń <adamsk91@wp.pl>
2024-08-06 21:07:50 +0200

    last is gallery

--- src/main.js
+++ src/main.js
@@ -1,17 +1,36 @@
+import iziToast from "izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+
 const form = document.querySelector("form");
 const input = document.querySelector(`input[type=text]`);
 const button = document.querySelector(`button[type=submit]`);
+const galleryLists = document.querySelector("ul");
+const loader = document.querySelector("div");
 
-const getPixaInfo = new URLSearchParams({
-  key: "33296379-dab9a6cf136214dcc7548d115",
-  q: input.value,
-  image_type: "photo",
-  orientation: "horizontal",
-  safesearch: true,
-});
+const reset = () => {
+  galleryLists.innerHTML = "";
+  input.value = "";
+}
 
 const getPixabay = e => {
   e.preventDefault();
+  const question = input.value;
+  reset();
+
+  if (galleryLists.innerHTML !== "") {
+    loader.classList.add("disable");
+  } else {
+    loader.classList.remove("disable");
+  }
+  
+  const getPixaInfo = new URLSearchParams({
+    key: "33296379-dab9a6cf136214dcc7548d115",
+    q: question,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  });
+
   fetch(`https://pixabay.com/api/?${getPixaInfo}`)
     .then((response) => {
       if (!response.ok) {
@@ -19,8 +38,36 @@ const getPixabay = e => {
       }
       return response.json();
     })
-    .then(post => console.log(post))
+    .then(gallery => {
+      const galleryLi = gallery.hits.map(image => {
+        return `<li>
+          <img src="${image.webformatURL}" alt="${image.tags}">
+          <p>Likes: ${image.likes}</p>
+          <p>Views: ${image.views}</p>
+          <p>Comments: ${image.comments}</p>
+          <p>Downloads: ${image.downloads}</p>
+        </li>`
+      })
+        .join("");
+      galleryLists.insertAdjacentHTML("beforeend", galleryLi);
+
+      if (galleryLists.innerHTML !== "") {
+        loader.classList.add("disable");
+      } else {
+        loader.classList.remove("disable");
+      }
+
+      if (gallery.total === 0) {
+        loader.classList.add("disable");
+        iziToast.show({
+          message: '❌Sorry, there are no images matching your search query. Please try again!',
+          color: 'red',
+          position: 'topRight'
+        })
+      }
+
+    })
     .catch(error => console.log(error));
 }
 
-form.addEventListener("submit", getPixabay)
+form.addEventListener("submit", getPixabay);
